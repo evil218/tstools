8f133143975e13e3e0b264a607998154e7b1394c
zhoucheng <zhoucheng@eb68d1f7-54e4-0b49-b8b5-a553322ddf86>
2010-09-25 05:47:15 +0000

    细化了install.bat，相应增加了uninstall.bat。
    git-svn-id: svn://192.165.54.31/tstool/trunk@1632 eb68d1f7-54e4-0b49-b8b5-a553322ddf86

--- release/install.bat
+++ release/install.bat
@@ -1,4 +1,9 @@
-copy /Y cyg*.dll c:\WINDOWS\system32
-copy /Y *.exe c:\WINDOWS\system32
+copy /Y cyggcc_s-1.dll c:\WINDOWS\system32
+copy /Y cygwin1.dll c:\WINDOWS\system32
+
+copy /Y bincat.exe c:\WINDOWS\system32
+copy /Y ipcat.exe c:\WINDOWS\system32
+copy /Y tsana.exe c:\WINDOWS\system32
+copy /Y tobin.exe c:\WINDOWS\system32
 
 pause
---
+++ release/uninstall.bat
@@ -0,0 +1,9 @@
+del c:\WINDOWS\system32\cyggcc_s-1.dll
+del c:\WINDOWS\system32\cygwin1.dll
+
+del c:\WINDOWS\system32\bincat.exe
+del c:\WINDOWS\system32\ipcat.exe
+del c:\WINDOWS\system32\tsana.exe
+del c:\WINDOWS\system32\tobin.exe
+
+pause
