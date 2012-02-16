274cd8b16cee2c388868fd98ef1035631efe215c
ZHOU_Cheng <zhoucheng@tsinghua.org.cn>
2012-02-16 13:54:19 +0800

    use new directory struct and new Makefile style, add AUTHORS LICENSE README @ root

--- release/windows/install.bat
+++
@@ -1,7 +0,0 @@
-copy /Y catts.exe c:\WINDOWS\system32
-copy /Y catip.exe c:\WINDOWS\system32
-copy /Y tsana.exe c:\WINDOWS\system32
-copy /Y tots.exe c:\WINDOWS\system32
-copy /Y toip.exe c:\WINDOWS\system32
-
-pause
--- release/windows/uninstall.bat
+++
@@ -1,7 +0,0 @@
-del c:\WINDOWS\system32\catts.exe
-del c:\WINDOWS\system32\catip.exe
-del c:\WINDOWS\system32\tsana.exe
-del c:\WINDOWS\system32\tots.exe
-del c:\WINDOWS\system32\toip.exe
-
-pause
